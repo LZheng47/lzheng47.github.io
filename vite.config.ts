4980c4e46a88e3303951a91dccae220e8e304457
SHUYESHUI\lzhen <57676919+LZheng47@users.noreply.github.com.>
2025-01-17 17:27:32 -0500

    Remove base config for vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/lzheng47.github.io/"
+  plugins: [react()]
 })
